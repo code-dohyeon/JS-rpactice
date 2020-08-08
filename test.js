475b792bf30d7f9d642e246739335ecbc5df3407
code-dohyeon <69374356+code-dohyeon@users.noreply.github.com>
2020-08-08 13:33:11 +0900

    JS_연습 오전 진행사항 업데이트

---
+++ test.js
@@ -0,0 +1,28 @@
+// --------------------alert 연습----------------------
+// console.log("김도현");
+// alert("이것은 경고창입니다.");
+
+// console.log(prompt());
+
+// let n = prompt();
+
+
+
+//------------3개 숫자 입력받아 더해 콘솔에 나타내기--------------
+// 1번째 방법 let n = Number(prompt());
+// 2번째 방법 n = Number(n);
+// 문자열을 숫자로 바꿔준다.
+// console.log(n+10);
+
+// let a = Number(prompt());
+ // let b = Number(prompt());
+ // let c = Number(prompt());
+
+// console.log(a+b+c);
+
+
+
+//---------prompt 입력받아 html에 입력하기-------------------
+// document.getElementById('h1').innerHTML = prompt();
+document.getElementById('h1').innerHTML += prompt();
+
